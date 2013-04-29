34a75abbb878bc9a3eb686e16f9b823da3af331d
Allan Roscoche <allanroscoche@gmail.com>
2013-04-29 08:24:41 -0300

    makefile

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include "csread.hpp"
 
 using std::cout;
 using std::endl;
 
-
 int main(){
   
-  cout << "Reading file..." << endl;
-  cout << "Read complete.." << endl;
+  CsRead read(5,(char *)"ACGTT");
+
+  cout << read << endl;
+
+  //cout << "Reading file..." << endl;
+  //cout << "Read complete.." << endl;
   
   return 0;
 }
