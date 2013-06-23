423cb2b4b12afa08d066a321ed8553eb4061a737
Allan Roscoche <allanroscoche@gmail.com>
2013-06-23 13:13:25 -0300

    kmerTable

---
+++ kmer.hpp
@@ -0,0 +1,15 @@
+#ifndef KMER
+#define KMER
+#include "globals.h"
+#include <iostream>
+#include <string>
+
+using std::istream;
+using std::ostream;
+
+class kmer {
+private:
+  int data;
+};
+
+#endif
---
+++ kmerTable.hpp
@@ -0,0 +1,15 @@
+#ifndef KMERTABLE
+#define KMERTABLE
+#include "globals.h"
+#include <iostream>
+#include <string>
+
+using std::istream;
+using std::ostream;
+
+class kmerTable {
+private:
+  int data;
+};
+
+#endif
